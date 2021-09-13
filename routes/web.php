d9642320b3d8e3ad9663cafd14ad967084e9f738
wendhaaldafa <wendha4429xi2@gmail.com>
2021-09-13 21:40:03 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+Route::get('/about', function () {
+    echo "Nama : Wendha<br>";
+    echo "NIM : 2031710154";
+});
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID " . $id;
+});
+
